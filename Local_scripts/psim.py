b0d173dabd74c54d1b4cc7169b0752de78b52899
Trambelus <trambelus2@gmail.com>
2015-07-11 20:23:53 -0400

    Documented psim.py

--- Local_scripts/psim.py
+++ Local_scripts/psim.py
@@ -14,8 +14,15 @@ import nltk
 USER = 'PloungerSimulator'
 APP = 'Simulator'
 STATE_SIZE = 2
+DBNAME = 'plounge.db3'
 
 class PText(markovify.Text):
+	"""
+	This subclass makes three changes: it modifies the sentence filter
+	to allow emotes in comments, it uses the Natural Language Toolkit
+	for slightly more coherent responses, and it guarantees a response
+	every time with make_sentence.
+	"""
 	def test_sentence_input(self, sentence):
 		"""
 		A basic sentence filter. This one rejects sentences that contain
@@ -48,7 +55,13 @@ class PText(markovify.Text):
 				return ret
 
 def get_markov(user):
+	"""
+	Given a user, return a Markov state model for them,
+	either from the cache or fresh from the database.
+	"""
 	try:
+		# Stores two files: some-reddit-user.txt for the raw corpus,
+		# and some-reddit-user.json for the structure holding the Markov state model.
 		f_txt = open('usermodels\%s.txt' % user, 'r')
 		f_json = open('usermodels\%s.json' % user, 'r')
 		text = ''.join(f_txt.readlines()).replace('~~','')
@@ -58,8 +71,10 @@ def get_markov(user):
 		f_txt.close()
 		f_json.close()
 		model = PText(text, state_size=STATE_SIZE, chain=markovify.Chain.from_json(json))
+	# This is a horrible way to determine if something is in a cache,
+	# but for some reason I was too lazy to google "python check if file exists".
 	except FileNotFoundError:
-		db = sqlite3.connect('plounge.db3')
+		db = sqlite3.connect(DBNAME)
 		res = db.execute("SELECT body FROM comments WHERE author LIKE '%s'" % user).fetchall()
 		if len(res) < 20:
 			return None
@@ -76,6 +91,9 @@ def get_markov(user):
 	return model
 
 def process(r, com, val):
+	"""
+	Multiprocessing target. Gets the Markov model, uses it to get a sentence, and posts that as a reply.
+	"""
 	target_user = val[val.find(' ')+1:]
 	if target_user[:3] == '/u/':
 		target_user = target_user[3:]
@@ -88,12 +106,17 @@ def process(r, com, val):
 		com.reply(reply)
 
 def monitor():
+	"""
+	Main loop. Looks through username notifications, comment replies, and whatever else,
+	and launches a single process for every new request it finds.
+	"""
 	started = []
 	req_pat = re.compile(r"\+/u/%s\s(/u/)?[\w\d\-_]{3,20}" % USER.lower())
 	r = rlogin.get_auth_r(USER, APP)
 	t0 = time.time()
 	while True:
 		try:
+			# Every 55 minutes, refresh the login.
 			if (time.time() - t0 > 55*60):
 				r = rlogin.get_auth_r(USER, APP)
 				t0 = time.time()
@@ -109,10 +132,19 @@ def monitor():
 				started.append(com.name)
 				Process(target=process, args=(r, com, res.group(0))).start()
 			time.sleep(1)
+		# General-purpose catch to make the script unbreakable.
 		except Exception as ex:
 			print("Error: %s" % ex)
 
 def manual(user):
+	"""
+	This allows the script to be invoked like this:
+	psim.py manual some-reddit-user
+	This was useful for when the script failed to reply in some cases.
+	I logged in as the script, got a manual response like this,
+	and just pasted it in as a normal comment.
+	They never knew.
+	"""
 	model = get_markov(user)
 	print(unidecode(model.make_sentence()))
 
