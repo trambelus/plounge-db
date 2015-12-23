da66c81ac7050f35e782ec397a9951244bba7302
Trambelus <trambelus2@gmail.com>
2015-12-23 02:39:01 -0500

    Added transcript logging attempt

--- Local_scripts/echo-server.py
+++ Local_scripts/echo-server.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 from flask import Flask
-html = """<script>
+html = """
+	<p id="transcript"></p>
+	<script>
 	recognition = new webkitSpeechRecognition();
 	//var speechSynthesis = new webkitSpeechSynthesis();
 
 	recognition.continuous = true;
 	recognition.interimResults = false;
+
+	var total_transcript = ''
 
 	recognition.onresult = function(event) {
 		var final_transcript = '';
@@ -15,6 +19,8 @@ html = """<script>
 			u.text = final_transcript;
 			u.lang = 'en-US';
 			speechSynthesis.speak(u);
+			total_transcript += '\n' + final_transcript;
+			document.getElementById("transcript").innerHTML = total_transcript;
 		}
 	};
 	recognition.start()
