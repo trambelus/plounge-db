c0453cb5b3ab97b33c0a86a7c834dadea6af4575
Trambelus <trambelus2@gmail.com>
2015-12-23 02:25:56 -0500

    Added echo server

---
+++ Local_scripts/echo-server.py
@@ -0,0 +1,29 @@
+from flask import Flask
+html = """<button type="button" onclick="recognition.start()">Start</button>
+<button type="button" onclick="recognition.stop()">Stop</button>
+
+<script>
+	speechSynthesis.speak(new SpeechSynthesisUtterance("Initialized"));
+	recognition = new webkitSpeechRecognition();
+	//var speechSynthesis = new webkitSpeechSynthesis();
+
+	recognition.continuous = true;
+	recognition.interimResults = false;
+
+	recognition.onresult = function(event) {
+		var final_transcript = '';
+		for (var i = event.resultIndex; i < event.results.length; ++i) {
+			final_transcript += event.results[i][0].transcript;
+			var u = new SpeechSynthesisUtterance();
+			u.text = final_transcript;
+			u.lang = 'en-US';
+			speechSynthesis.speak(u);
+		}
+	};
+</script>"""
+app = Flask(__name__)
+@app.route('/')
+def default():
+	return html
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80)
